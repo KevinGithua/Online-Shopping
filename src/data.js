be4acb345dafe1fcd359b3fe1a7964f769fc9c59
KevinGithua <githuakevinnjuguna@gmail.com>
2022-10-10 13:35:22 +0300

    version 1

---
+++ src/data.js
@@ -0,0 +1,50 @@
+let shopItemsData = [{
+    id: "object1",
+    name: "Ipad",
+    price: 1000,
+    desc: "Ipad Pro 11 2022 model",
+    img: "product images/ipad.jpg"
+},
+{
+    id: "object2",
+    name: "Bags",
+    price: 45,
+    desc: "Slick cotton bags with cool colors",
+    img: "product images/bags.jpg"
+},
+{
+    id: "object3",
+    name: "Iphone",
+    price: 1200,
+    desc: "Iphone 12 pro",
+    img: "product images/iphone.jpg"
+},
+{
+    id: "object4",
+    name: "Redmi",
+    price: 980,
+    desc: "Redmi note 11 Pro",
+    img: "product images/redmi.jpg"
+},
+{
+    id: "object5",
+    name: "Samsung",
+    price: 1500,
+    desc: "Samsung S22 Ultra",
+    img: "product images/samsung.jpg"
+},
+{
+    id: "object6",
+    name: "Hp Laptop",
+    price: 2000,
+    desc: "Hp Slimpack core i9 with Linux OS",
+    img: "product images/laptop.jpg"
+},
+{
+    id: "object7",
+    name: "MacBook Air",
+    price: 1500,
+    desc: "MacBook Air Pro with high tech M2 processor",
+    img: "product images/macbook.jpg",
+}
+];
